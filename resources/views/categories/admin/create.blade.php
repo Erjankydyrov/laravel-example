2326620dab88106596855de6dbe8b1960bd2bb8f
Erjankydyrov <erjankydyrov29@gmail.com>
2023-10-26 10:31:30 +0600

    added new styles to category create page

--- resources/views/categories/admin/create.blade.php
+++ resources/views/categories/admin/create.blade.php
@@ -5,25 +5,27 @@
 @extends('layouts.app')
 
 @section('main')
-    <h1>Создание новой категории</h1>
+    <h1 class="create-category-title">Создание новой категории</h1>
     <form method="post" action="{{ route('categories.store') }}" enctype="multipart/form-data" class="category-create-form">
         @csrf
         <div class="form-group">
             <label for="name">Название категории:</label>
-            <input type="text" id="name" name="name" required>
+            <input type="text" id="name" name="name" class="create-form-control" required>
         </div>
         <div class="form-group">
             <label for="description">Описание категории:</label>
-            <textarea id="description" name="description" required></textarea>
+            <textarea id="description" name="description" class="create-form-control" required></textarea>
         </div>
         <div class="form-group">
             <label for="image">Изображение категории:</label>
-            <input type="file" id="image" name="image" class="image-create" required>
+            <input type="file" id="image" name="image" class="image-create" class="create-form-control" required>
             <img id="image-preview" class="image-preview" src="" alt="">
         </div>
         <button type="submit" class="create-button">Создать категорию</button>
     </form>
-    <a href="{{ route('categories.adminIndex') }}" class="back-link">Назад к списку категорий</a>
+    <div class="create-back-link-box">
+        <a href="{{ route('categories.adminIndex') }}" class="back-link">Назад к списку категорий</a>
+    </div>
 @endsection
 
-<script src="{{ asset('js/categories_create.js')}}"></script>
+<script src="{{ asset('js/categories_create.js') }}"></script>
